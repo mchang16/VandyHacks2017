8d106ad375e1bc87e4223eb7938b0808b38cf5aa
Althea <asmith790@ufl.edu>
2017-10-21 14:36:34 -0400

    updated new img URL

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 //alert("Lets make some memes!")
 var meme_num = Math.floor((Math.random() * 80) + 1);
 var meme = "memes/%20(" + meme_num + ").jpg";
- 
+
 
 //alert(meme);
 function img_find() {
     "use strict";
     $(document).ready(function () {
-        $('img').attr('src', chrome.extension.getURL('cats.jpg'));
+        $('img').attr('src', chrome.extension.getURL('meme'));
     });
 }
 img_find();
