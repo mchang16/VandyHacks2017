872356f7579e8b978b848ad7434c666f54171343
Althea <asmith790@ufl.edu>
2017-10-21 12:22:06 -0400

    Changed window size

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 //alert("Lets make some memes!")
 
 function img_find() {
+  alert("Running img_find");
     var imgs = document.getElementsByTagName("img");
     //var imgSrcs = [];
     for (var i = 0; i < imgs.length; i++) {
