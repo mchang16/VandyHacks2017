42836924cf2c65b998ee42baf5c4ea443b98c809
mchang16 <mchang0816@gmail.com>
2017-10-21 14:28:18 -0500

    Update

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
 //alert("Lets make some memes!")
-<<<<<<< HEAD
 var meme_num = Math.floor((Math.random() * 80) + 1);
 var meme = "memes/%20(" + meme_num + ").jpg";
+
 var button = document.querySelector('button');
 
 button.onclick = img_find;
-=======
 
  
->>>>>>> aa6ff8b09c314f950675d8962ef08027622ff5cf
 
 //alert(meme);
 function img_find() {
