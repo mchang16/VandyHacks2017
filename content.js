1efd1673ee10a8fda6d479b2a7b3e074be3848aa
mchang16 <mchang0816@gmail.com>
2017-10-21 14:22:56 -0500

    Work on button

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 //alert("Lets make some memes!")
 var meme_num = Math.floor((Math.random() * 80) + 1);
 var meme = "memes/%20(" + meme_num + ").jpg";
+var button = document.querySelector('button');
 
+button.onclick = img_find;
 
 //alert(meme);
 function img_find() {
