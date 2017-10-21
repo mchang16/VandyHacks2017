aa6ff8b09c314f950675d8962ef08027622ff5cf
Ryan Zeng <zeng.ryan2@gmail.com>
2017-10-21 15:13:35 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,21 @@
 //alert("Lets make some memes!")
-var meme_num = Math.floor((Math.random() * 80) + 1);
-var meme = "memes/%20(" + meme_num + ").jpg";
 
+ 
 
 //alert(meme);
 function img_find() {
     "use strict";
-    $(document).ready(function () {
-        $('img').attr('src', chrome.extension.getURL('memes/%20(1).jpg'));
+    
+    $(document).delay(3000).ready(function () {
+        var count = $('body img').length;
+    
+        for (var i = 0; i <= count; i++){
+            var img_elem = $('img').get(i);
+            var meme_num = Math.floor((Math.random() * 86) + 1);
+            var meme = "memes/%20(" + meme_num + ").jpg";
+            $(img_elem).attr('src', chrome.extension.getURL(meme))
+        }
     });
+
 }
 img_find();
