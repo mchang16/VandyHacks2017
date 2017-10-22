dd46b228efadb372ddd6c89beecba2809f31d097
mchang16 <mchang0816@gmail.com>
2017-10-21 22:42:34 -0500

    memeify

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-$("#onOff").click(img_find());
+// Retrieves from popup.js
+chrome.extension.onConnect.addListener(function(port) {
+    port.onMessage.addListener(function(msg) {
+        
+        //Communicate with contents.js
+         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+           chrome.tabs.sendMessage(tabs[0].id, {greeting: "memeify"}, function(response) {
+           });
+         });
+     });
+});
