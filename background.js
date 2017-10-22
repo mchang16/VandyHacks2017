2ee70a1d75a292b3c8076c28b66dfec859e0730d
Ryan Zeng <zeng.ryan2@gmail.com>
2017-10-22 01:17:24 -0400

    Update background.js

--- background.js
+++ background.js
@@ -14,20 +14,12 @@ chrome.extension.onConnect.addListener(function(port) {
              //Communicate with contents.js
               chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {greeting: "memeify"}, function(response) {
+                        port.onMessage.addListener(function(msg) {
                 });
               });
-             }
+             });
+            }
          }
-      });
  });
 
-
-    port.onMessage.addListener(function(msg) {
-        
-        //Communicate with contents.js
-         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-           chrome.tabs.sendMessage(tabs[0].id, {greeting: "memeify"}, function(response) {
-           });
-         });
-     });
 });
