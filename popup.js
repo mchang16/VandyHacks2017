57c69100611dfb9b3c081e5a4b7791f654aef4b7
Ryan Zeng <zeng.ryan2@gmail.com>
2017-10-22 09:44:53 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,38 +1,50 @@
-var isOn = true;
 
-$(document).ready(function(){
+
+$(document).ready(function () {
+    
+    var saved_option = localStorage["save_option"];
+    if (saved_option == 'off'){
+        var isOn = true;
+        $("#onOff").text("Turn On");
+    }else{
+        var isOn = false    ;
+        $("#onOff").text("Turn Off");
+    }
+
+    
     $("#makeMeme").click(function () {
-        
         // Communicate with Background.js
         var port = chrome.extension.connect({
             name: "Memeify "
         });
             port.postMessage("memeify");
-            port.onMessage.addListener(function(msg) {
+            port.onMessage.addListener(function (msg) {
         });
     });
     
     // Click Turn on/off switch
-    $("#onOff").click(function(){
+    $("#onOff").click(function () {
         // Change button text on and off.
         // Calls background.js with specific messages
-        if (!isOn){
+        if (!isOn) {
             $(this).text('Turn Off');
             isOn = true;
+            localStorage["save_option"] = "on";
             var port = chrome.extension.connect({
                name: "Meme On" 
             });
             port.postMessage("memeOn");
-            port.onMessage.addListener(function(msg){
+            port.onMessage.addListener(function (msg) {
             });
-        }else{
+        } else {
             $(this).text('Turn On');
             isOn = false;
+            localStorage["save_option"] = "off";
             var port = chrome.extension.connect({
                 name: "Meme Off"
             });
             port.postMessage("memeOff");
-            port.onMessage.addListener(function(msg){
+            port.onMessage.addListener(function(msg) {
             });
         }
     });
