b8ffa079baee8f6721d226b3465d53528ebdb434
Ryan Zeng <zeng.ryan2@gmail.com>
2017-10-21 21:19:11 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,15 +1,9 @@
-//alert("Lets make some memes!")
-var meme_num = Math.floor((Math.random() * 80) + 1);
-var meme = "memes/%20(" + meme_num + ").jpg";
+"use strict";
 
-$(".onOff").click(img_find());
-//alert(meme);
 function img_find() {
-    "use strict";
-    
-    $(document).delay(3000).ready(function () {
+    $(document).ready(function () {
         var count = $('body img').length;
-    
+
         for (var i = 0; i <= count; i++){
             var img_elem = $('img').get(i);
             var meme_num = Math.floor((Math.random() * 86) + 1);
@@ -17,6 +11,15 @@ function img_find() {
             $(img_elem).attr('src', chrome.extension.getURL(meme))
         }
     });
-
 }
 img_find();
+
+// Retrieves from background.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "memeify"){
+        sendResponse({farewell: "goodbye"});
+        img_find();
+    }
+});
+
